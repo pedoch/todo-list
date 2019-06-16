69cf366e3a374c252c38c8eae48a29fe0f316795
Pedro Ogheneochuko <pedroogheneochuko@gmail.com>
2019-06-16 19:02:43 +0100

    Updates

---
+++ precache-manifest.b7d24dabfadf257b02cd02cfd913d262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2767408471922a8fb709570a13caf85e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "849cfcbb615dca6ffee7",
+    "url": "/todo-list/static/css/main.50e0d94b.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/todo-list/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "849cfcbb615dca6ffee7",
+    "url": "/todo-list/static/js/main.a2bda680.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
